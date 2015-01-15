21b287d8be25a60eb9ba5b7b863498904e9cb47a
Jose Espinosa <kadomu@gmail.com>
2015-01-15 15:20:30 +0100

    Authorship is also important

--- src/org/crg/utils/maths/LogBase.java
+++ src/org/crg/utils/maths/LogBase.java
@@ -1,5 +1,11 @@
 package org.crg.utils.maths;
 
+/**
+ * Logarithm in any base
+ * @author jespinosa-carrasco
+ * @date 2015-Jan-15
+ */
+
 public class LogBase {
 	public static double LogBase( double a, double b ) {
 		return Math.log(a) / Math.log(b);
